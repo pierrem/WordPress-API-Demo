9adeac6fa2c6e2535ada3b78778b549fd1c8aa6f
Pierre Marty <p.marty@me.com>
2015-02-11 16:56:05 +0100

    XCode project created

---
+++ WordPress-API/WordPress-APITests/WordPress_APITests.swift
@@ -0,0 +1,36 @@
+//
+//  WordPress_APITests.swift
+//  WordPress-APITests
+//
+//  Created by Pierre Marty on 11/02/2015.
+//  Copyright (c) 2015 Pierre Marty. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class WordPress_APITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

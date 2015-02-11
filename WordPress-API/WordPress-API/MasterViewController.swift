7f59499b0f765657fb5d2e1b9bf2b74ea39fac7a
Pierre Marty <p.marty@me.com>
2015-02-11 22:25:17 +0100

    typo

--- WordPress-API/WordPress-API/MasterViewController.swift
+++ WordPress-API/WordPress-API/MasterViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 class MasterViewController: UITableViewController {
-
+    
     let webService = WordPressWebServices(url:"https://public-api.wordpress.com/rest/v1.1/sites/www.alpeslog.com")
     
     var categories = [Dictionary<String, AnyObject>]()
-
+    
     var detailViewController: DetailViewController? = nil
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
@@ -23,15 +23,15 @@ class MasterViewController: UITableViewController {
             self.preferredContentSize = CGSize(width: 320.0, height: 600.0)
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if let split = self.splitViewController {
             let controllers = split.viewControllers
             self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
         }
-
+        
         webService.categories({ (categories, error) -> Void in
             if categories != nil {
                 self.categories = categories
@@ -40,10 +40,10 @@ class MasterViewController: UITableViewController {
                 })
             }
         })
-}
-
+    }
+    
     // MARK: - Segues
-
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow() {
@@ -55,31 +55,31 @@ class MasterViewController: UITableViewController {
             }
         }
     }
-
+    
     // MARK: - Table View
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories.count
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
-
+        
         let category = categories[indexPath.row]
         cell.textLabel!.text = category["name"] as? String
         return cell
     }
-
+    
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
-
-
+    
+    
+    
 }
 
